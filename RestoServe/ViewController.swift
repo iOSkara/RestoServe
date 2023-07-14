dfedaf6db89c2713230f9ddf08ce6d0c7a99e587
Roman Vasyltsov <rgamer402@gmail.com>
2023-07-14 15:40:17 +0300

    Initial Commit

---
+++ RestoServe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RestoServe
+//
+//  Created by Roman Vasyltsov on 14.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
